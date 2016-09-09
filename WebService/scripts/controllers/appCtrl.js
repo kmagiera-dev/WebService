b26295932243d5eb43ad1bcdc445fc0fe7fad448
Krzysztof Magiera <krzysztof.magiera@codeconcept.pl>
2016-09-09 13:11:27 +0200

    create grid & date formatting

--- WebService/scripts/controllers/appCtrl.js
+++ WebService/scripts/controllers/appCtrl.js
@@ -1,4 +1,33 @@
 ﻿var app = angular.module("app", []);
+window.onload = function () {
+
+    // create the grid
+    var grid = new wijmo.grid.FlexGrid('#theGrid');
+
+    // get some data
+    wijmo.httpRequest('WebService.asmx/HelloWorld?$format=json', {
+        success: function (xhr) {
+            var response = JSON.parse(xhr.response, dateTimeReviver);
+            grid.itemsSource = response;
+        }
+    });
+
+}
+
+dateTimeReviver = function (key, value) {
+    var a;
+    if (key == 'Date') {
+        a = /\/Date\((-?\d*)\)\//.exec(value);
+        if (a) {
+            var d = new Date(+a[1]);
+            var str = d.getFullYear() + "-" + ('0' + (d.getMonth() + 1)).slice(-2) + "-" + ('0' + d.getDate()).slice(-2) + " " +
+                ('0' + d.getHours()).slice(-2) + ":" + ('0' + d.getMinutes()).slice(-2) + ":" + ('0' + d.getSeconds()).slice(-2) + ":" + ('00' + d.getMilliseconds()).slice(-3);
+            return str;
+        }
+    }
+    return value;
+}
+
 
 app.controller("appCtrl", function($scope, $http) {
     function getData() {
